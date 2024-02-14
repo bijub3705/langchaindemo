88a39aead8bbf8be5a32dbbcfe99ff0ae07ad887
vidhya2192 <bijunb3705@gmail.com>
2024-02-14 13:07:34 -0500

    updated

--- src/claims/prompt/__init__.py
+++ src/claims/prompt/__init__.py
@@ -1,9 +1,13 @@
 from langchain_core.prompts import PromptTemplate
 
-template = """Instructions: Use only the following context to answer the question.
+template = """You are a helpful AI assistant. 
+    Use the following context to answer the question at the end. Stop when you've answered the question. Do not generate any more than that.
+    Respond "Please elaborate more" when you are unable to find an answer.
+        
+    Consider the following information to answer questions:
 
-Context: {context}
-Question: {question}
+    Context: {context}
+    Question: {question}
 """
 def get_prompt():
     return PromptTemplate(template=template, input_variables=["context", "question"])
